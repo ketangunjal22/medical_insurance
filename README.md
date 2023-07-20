5ba18598ed98b5f844574640ef2c2078292bf5a2
payalgunjal <payalgunjal02@gmail.com>
2023-07-20 19:33:55 +0530

    deleted all files in testingfile

--- README.md
+++
@@ -1,2 +0,0 @@
-# medical_insurance
-this project is to predict the insurance prices with the help of some factors. basically it is regression problem.
--- config.py
+++
@@ -1,5 +0,0 @@
-MODEL_FILE_PATH = r"artifacts\model.pkl"
-
-CSV_FILE_PATH = r"artifacts\medical_insurance.csv"
-
-JSON_DATA_PATH = r"artifacts\json_data.json"
